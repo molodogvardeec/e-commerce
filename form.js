adbe75de408d1b851dbbe880a6beee0dab120aa3
molodogvardeec <artem230200@mail.ru>
2024-06-19 15:10:08 +0500

    first commit

--- form.js
+++ form.js
@@ -2,40 +2,64 @@ const signUp = document.querySelector(".header__buttons-signUp");
 const elemSign = document.querySelector(".sign-up");
 const elemLogin = document.querySelector(".sign-in");
 const logIn = document.querySelector(".header__buttons-login");
-const Close = document.querySelector(".closeX");
-const Close2 = document.querySelector(".closeX2");
+const closeSign = document.querySelector(".closeX");
+const closeLogin = document.querySelector(".closeX2");
 const modal = document.querySelector(".sign-up__wrapper");
 
+const toggleBodyOverflow = () => {
+  if (document.body.style.overflow === "hidden") {
+    document.body.style.overflow = "auto";
+  } else {
+    document.body.style.overflow = "hidden";
+  }
+};
+
+const toggleElemSignDisplay = () => {
+  if (elemSign.style.display === "block") {
+    elemSign.style.display = "none";
+  } else {
+    elemSign.style.display = "block";
+  }
+};
+
+const toggleElemLoginDisplay = () => {
+  if (elemLogin.style.display === "block") {
+    elemLogin.style.display = "none";
+  } else {
+    elemLogin.style.display = "block";
+  }
+};
+
 signUp.addEventListener("click", () => {
-  elemSign.style.display = "block";
-  document.body.style.overflow = "hidden";
+  toggleElemSignDisplay();
+  toggleBodyOverflow();
 });
 
 logIn.addEventListener("click", () => {
-  elemLogin.style.display = "block";
-  document.body.style.overflow = "hidden";
+  toggleElemLoginDisplay();
+  toggleBodyOverflow();
 });
 
-Close.addEventListener("click", () => {
-  elemSign.style.display = "none";
-  document.body.style.overflow = "auto";
+closeSign.addEventListener("click", () => {
+  toggleElemSignDisplay();
+  toggleBodyOverflow();
 });
 
-Close2.addEventListener("click", () => {
-  elemLogin.style.display = "none";
-  document.body.style.overflow = "auto";
+closeLogin.addEventListener("click", () => {
+  toggleElemLoginDisplay();
+  toggleBodyOverflow();
 });
 
 elemSign.addEventListener("click", (e) => {
   if (!e.target.id == false) {
-    elemSign.style.display = "none";
-    document.body.style.overflow = "auto";
+    toggleElemSignDisplay();
+    toggleBodyOverflow();
   }
 });
 
 elemLogin.addEventListener("click", (e) => {
   if (!e.target.id == false) {
-    elemLogin.style.display = "none";
-    document.body.style.overflow = "auto";
+    toggleElemLoginDisplay();
+    toggleBodyOverflow();
   }
 });
